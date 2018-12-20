8eb9d71a92bc1799931ea3faee08367b1cf4f2e3
GuoXinjian <42989601+GuoXinjian@users.noreply.github.com>
2018-12-20 17:34:17 +0800

    liujiang-login

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . import models
 # Create your views here.
+
+def index(request):
+    pass
+    return render(request,'login/index.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+        message = "所有字段必须填写"
+        if username and password:
+            username = username.strip()
+            #用户名字符合法性验证
+            #密码长度验证
+            #其他验证
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    print(username,password)
+                    return redirect('/index/')
+                else:
+                    message="密码错误"
+            except:
+                message = '用户不存在'
+        return render(request,'login/login.html',{'message':message})
+            
+    return render(request,'login/login.html')
+
+def register(request):
+    pass
+    return render(request,'login/register.html')
+
+def logout(request):
+    pass
+    return render('index/')
