60b804b52a596dd7a40b9ac2daee218c10f5bb10
GuoXinjian <42989601+GuoXinjian@users.noreply.github.com>
2018-12-20 15:30:16 +0800

    mysite-login

---
+++ mysite/login/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    gender =(
+        ('male','男'),
+        ('female','女'),
+    )
+
+    name = models.CharField(max_length = 256,verbose_name='用户名')
+    password = models.CharField(max_length = 256，verbose='密码')
+    email = models.EmailField(unique=True)
+    sex = models.CharField(max_length=32, choices=gender,default='男')
+    c_time = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-c_time']
+        verbose_name = '用户'
+        verbose_name_plural = '用户'
