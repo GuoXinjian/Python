4f558793563c93f062b42d36d779d592d6e1efef
unknown <follow_wind>
2019-03-03 17:18:25 +0800

    scrapy

--- KTS/KTS/items.py
+++ KTS/KTS/items.py
@@ -12,3 +12,19 @@ class KtsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class WeiboUserItem(scrapy.Item):
+    user_id = scrapy.Field()
+    user_name = scrapy.Field()
+    user_url = scrapy.Field()
+    statuses_count = scrapy.Field()
+    verified = scrapy.Field()
+    verified_type = scrapy.Field()
+    verified_reason = scrapy.Field()
+    description = scrapy.Field()
+    gender = scrapy.Field()
+    followers_count = scrapy.Field()
+    follow_count = scrapy.Field()
+    avatar_hd = scrapy.Field()
+
+    
