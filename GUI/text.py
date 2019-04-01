2cc38249b3f0c441f4c6b68928fc47e58040295d
GuoXinjian <42989601+GuoXinjian@users.noreply.github.com>
2019-04-01 18:55:37 +0800

    20190401

--- GUI/text.py
+++ GUI/text.py
@@ -1,13 +1,4 @@
-import msvcrt,time
-
-
-n=0
-while True:
-    n+=1
-    print(n)
-    time.sleep(2)
-    if ord(msvcrt.getch())>50:
-        break
+import danmu
 
 
 # print(time.strftime('%Y%m%d',time.localtime(time.time())))
