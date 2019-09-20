c2cc39cc49f1530f42774e49c5ea2b1bb95ffe14
GuoXinjian <42989601+GuoXinjian@users.noreply.github.com>
2019-09-20 16:58:25 +0800

    20190920

--- test.py
+++ test.py
@@ -1,20 +1,29 @@
-from sqlalchemy import Column, String, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import requests
 
-# 创建对象的基类:
-Base = declarative_base()
+url = 'http://www.fangline.cn/gather.do?method=listResources'
 
-# 定义User对象:
-class User(Base):
-    # 表的名字:
-    __tablename__ = 'user'
 
-    # 表的结构:
-    id = Column(String(20), primary_key=True)
-    name = Column(String(20))
 
-# 初始化数据库连接:
-engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/test')
-# 创建DBSession类型:
-DBSession = sessionmaker(bind=engine)
+headers = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,en-US;q=0.8',
+    'User-Agent':'Mozilla/5.0 (Linux; Android 5.1.1; vivo X9Plus Build/LMY48Z) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Safari/537.36 MicroMessenger/6.6.7.1321(0x26060736) NetType/WIFI Language/zh_CN',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Cookie': 'SESSION=51c45b10-6ecb-403d-82a1-297cd4142028; ant.mobile.wx46cbc2feb419e889=onWKSweQELTTRDKEdUqsccGkFqF4; SERVERID=b1c2ca27bdcbfbfd61e99ab704c80e0f|1567736969|1567736760'
+
+}
+
+i=1
+while i<5:
+    data = {
+        'pageNo':'1',
+        'pageSize':'25',
+        'bizType':'sell',
+        'searchType':'1',
+        'status':'3,2,4',
+        'orderCol':'createTime',
+    }
+    res=requests.post(url, data=data,headers=headers)
+    print(res.content)
+    i+=1
